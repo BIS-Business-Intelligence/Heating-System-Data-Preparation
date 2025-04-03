c4cfee5a668aa98ec54d7d01129e212425cdf247
Leandro Hoenen <leandrohoenen@MacStudio-von-Leandro.local>
2025-04-03 20:28:56 +0200

    outcommented code for building 2

--- main.py
+++ main.py
@@ -12,23 +12,30 @@ output_dir_name = "data/output/"
 # Data Cleaning and Merging
 # -------------------------
 
-b2_data = dc_helper.load_building_data("Building 2", input_dir_name, "Building 2")
+# b2_data = dc_helper.load_building_data("Building 2", input_dir_name, "Building 2")
 b3_data = dc_helper.load_building_data("Building 3", input_dir_name, "Building 3")
-
-all_buildings = pd.concat([b2_data, b3_data], ignore_index=True)
+b3_data["SupplyTempReturnTempDerivation"] = abs(b3_data["SupplyTemp"] - b3_data["ReturnTemp"])
+# all_buildings = pd.concat([b2_data, b3_data], ignore_index=True)
 
 dir_helper.create_directory(output_dir_name)
 
 print("Export of Merging dataset to 'all_buildings_merged.csv' has started.")
-all_buildings.to_csv(f"{output_dir_name}all_buildings_merged.csv", index=False)
+# all_buildings.to_csv(f"{output_dir_name}all_buildings_merged.csv", index=False)
+# all_buildings.to_csv(f"{output_dir_name}all_buildings_merged.csv", index=False)
+b3_data.to_csv(f"{output_dir_name}all_buildings_merged.csv", index=False)
+b3_data.to_csv(f"{output_dir_name}all_buildings_merged.csv", index=False)
 print("Merged dataset exported as 'all_buildings_merged.csv'.")
 
 # -------------------------
 # Star Schema Creation
 # -------------------------
-dim_building = star_helper.create_dim_building(all_buildings)
-dim_time = star_helper.create_dim_time(all_buildings)
-fact_measurements = star_helper.create_fact_measurements(all_buildings, dim_building, dim_time)
+# dim_building = star_helper.create_dim_building(all_buildings)
+# dim_time = star_helper.create_dim_time(all_buildings)
+# fact_measurements = star_helper.create_fact_measurements(all_buildings, dim_building, dim_time)
+
+dim_building = star_helper.create_dim_building(b3_data)
+dim_time = star_helper.create_dim_time(b3_data)
+fact_measurements = star_helper.create_fact_measurements(b3_data, dim_building, dim_time)
 
 print("Star schema CSV tables export has started.")
 dim_building.to_csv(f"{output_dir_name}dim_building.csv", index=False)
@@ -44,3 +51,4 @@ with pd.ExcelWriter(excel_filename) as writer:
     dim_time.to_excel(writer, sheet_name="DIM_TIME", index=False)
     fact_measurements.to_excel(writer, sheet_name="FACT_MEASUREMENTS", index=False)
 print(f"Star schema Excel file exported as '{excel_filename}'.")
+
