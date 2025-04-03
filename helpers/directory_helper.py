ddc8e44743397a1d5cbbbec57dcee76e66cf1915
Leandro Hoenen <leandrohoenen@MacStudio-von-Leandro.local>
2025-04-03 19:27:47 +0200

    Added method description for create dir

--- helpers/directory_helper.py
+++ helpers/directory_helper.py
@@ -1,6 +1,9 @@
 import os
 
 def create_directory(path: str):
+    """
+    Creates a directory if it does not exist.
+    """
     if os.path.exists(path):
         print(f"Directory '{path}' already exists.")
         return
